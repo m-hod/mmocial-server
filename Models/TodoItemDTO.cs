f856741f30304c0ea45c61d58a2d0c72edc57cd2
m-hodges <50115036+m-hodges@users.noreply.github.com>
2021-04-29 08:16:39 +1200

    initial commit

---
+++ Models/TodoItemDTO.cs
@@ -0,0 +1,9 @@
+namespace mmocial_server.Models
+{
+    public class TodoItemDTO
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
